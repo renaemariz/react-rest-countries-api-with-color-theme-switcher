0c1284e0b709b444ef700c5c536ca424aaa50422
renaemariz <renaemarizcarino@gmail.com>
2021-02-02 18:00:50 +0800

    commit new and modified files

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const SearchBar = ({keyword, handleOnChange}) => {
+const SearchBar = ({handleOnChange}) => {
 	return (
 		<div className="searchbar rounded">
 			<button className="searchbar-btn">
 				<i className="fas fa-search"></i>
 			</button>
-			<input type="text" className="searchbar-input shadow-overlay" placeholder="Search for a country" name="country" value={keyword} onChange={(e) => handleOnChange(e.target.value)} />
+			<input type="text" className="searchbar-input shadow-overlay" placeholder="Search for a country" name="country" defaultValue="" onChange={(e) => handleOnChange(e.target.value)} />
 		</div>
 	)
 }
